90b51343dd21ea5bc03ff9680e98cd0d665bd00e
xeuva <xeuva@outlook.com>
2024-10-29 00:43:30 +0800

    refactor: clean unused codes

--- src/background.js
+++ src/background.js
@@ -1,62 +1,62 @@
-chrome.runtime.onInstalled.addListener(() => {
-  const RULE = [
-    {
-      "id": 1,
-      "priority": 1,
-      "action": {
-        "type": "modifyHeaders",
-        "requestHeaders": [
-          {
-            "header": "referer",
-            "operation": "set",
-            "value": "https://www.pixiv.net/"
-          }
-        ]
+(async () => {
+  chrome.runtime.onInstalled.addListener(() => {
+    const RULE = [
+      {
+        "id": 1,
+        "priority": 1,
+        "action": {
+          "type": "modifyHeaders",
+          "requestHeaders": [
+            {
+              "header": "referer",
+              "operation": "set",
+              "value": "https://www.pixiv.net/"
+            }
+          ]
+        },
+        "condition": {
+          initiatorDomains: [chrome.runtime.id],
+          "urlFilter": "pixiv.net",
+          "resourceTypes": [
+            "xmlhttprequest",
+          ]
+        }
       },
-      "condition": {
-        initiatorDomains: [chrome.runtime.id],
-        "urlFilter": "pixiv.net",
-        "resourceTypes": [
-          "xmlhttprequest",
-        ]
+      {
+        "id": 2,
+        "priority": 1,
+        "action": {
+          "type": "modifyHeaders",
+          "requestHeaders": [
+            {
+              "header": "referer",
+              "operation": "set",
+              "value": "https://www.pixiv.net/"
+            }
+          ]
+        },
+        "condition": {
+          initiatorDomains: [chrome.runtime.id],
+          "urlFilter": "pximg.net",
+          "resourceTypes": [
+            "xmlhttprequest",
+          ]
+        }
       }
-    },
-    {
-      "id": 2,
-      "priority": 1,
-      "action": {
-        "type": "modifyHeaders",
-        "requestHeaders": [
-          {
-            "header": "referer",
-            "operation": "set",
-            "value": "https://www.pixiv.net/"
-          }
-        ]
-      },
-      "condition": {
-        initiatorDomains: [chrome.runtime.id],
-        "urlFilter": "pximg.net",
-        "resourceTypes": [
-          "xmlhttprequest",
-        ]
-      }
-    }
-  ];
-  chrome.declarativeNetRequest.updateDynamicRules({
-    removeRuleIds: RULE.map(o => o.id),
-    addRules: RULE,
+    ];
+    chrome.declarativeNetRequest.updateDynamicRules({
+      removeRuleIds: RULE.map(o => o.id),
+      addRules: RULE,
+    });
   });
-});
 
-(async function () {
   function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min);
   }
   class Queue {
-    constructor(maxsize = 5) {
+    constructor(maxsize) {
       this.maxsize = maxsize;
       this.array = [];
     }
@@ -85,227 +85,27 @@ chrome.runtime.onInstalled.addListener(() => {
       return false;
     }
   }
-  class illustSource extends Queue {
-    constructor(maxsize = 1) {
-      super(maxsize);
-      this.baseUrl = "https://www.pixiv.net";
-      this.illustInfoUrl = "/ajax/illust/";
-      this.maxRetries = 10;
-      this.running = 0;
+
+  async function fetchPixivJson(url) {
+    let res = await fetch(url);
+    let res_json = await res.json();
+    if (res_json.error == true) {
+      console.error(`Fetch pixiv json error: ${res_json}`);
     }
-    async fetchJson(url) {
-      return fetch(url, { signal: AbortSignal.timeout(5000) })
-        .then((res) => res.json())
-        .then((res) => {
-          return new Promise((resolve, reject) => {
-            if (res.error === true) {
-              reject(res);
-            } else {
-              resolve(res);
-            }
-          });
-        });
-    }
-    async fetchImage(url) {
-      return fetch(url, { signal: AbortSignal.timeout(5000) })
-        .then((res) => res.blob());
-    }
-  }
-  class DiscoverySource extends illustSource {
-    constructor(maxsize = 200) {
-      super(maxsize);
-      // this.url = "/ajax/discovery/artworks?mode=all&limit=100";
-      this.url = "/ajax/discovery/artworks?mode=all";
-    }
-    async getItem() {
-      let item = this.pop();
-      if (item === undefined) {
-        let jsonResult = await this.fetchJson(this.baseUrl + this.url);
-        let A = jsonResult.body.thumbnails.illust;
-        let tasks = [];
-        for (let i = 0; i < A.length; ++i) {
-          tasks.push(this.fetchJson(this.baseUrl + this.illustInfoUrl + A[i].id)
-            .then((illustInfo, reject) => {
-              if (illustInfo.body.bookmarkCount + illustInfo.body.likeCount < 10000) {
-                reject("too small bookmarks and likes");
-              } else {
-                this.push({
-                  userName: illustInfo.body.userName,
-                  userId: illustInfo.body.userId,
-                  illustId: illustInfo.body.illustId,
-                  userIdUrl: this.baseUrl + "/users/" + illustInfo.body.userId,
-                  illustIdUrl: this.baseUrl + "/artworks/" + illustInfo.body.illustId,
-                  title: illustInfo.body.title,
-                  profileImageUrl: A[i].profileImageUrl,
-                  imageObjectUrl: illustInfo.body.urls.regular,
-                });
-              }
-            })
-          );
-        }
-        await Promise.any(tasks);
-        item = this.pop();
-      }
-      return item;
-    }
+    return res_json;
   }
 
-  class SearchSource extends illustSource {
-    constructor(maxsize = 2) {
-      super(maxsize);
-      this.illustInfoPages = {};
-      // this.positiveTagArray = [
-      //   "オリジナル",
-      //   "女の子",
-      //   // "漫画",
-      //   "東方",
-      //   // "落書き",
-      //   // "アナログ",
-      //   // "創作",
-      //   "艦これ",
-      //   // "ポケモン",
-      //   // "イラスト",
-      //   // "艦隊これくしょん",
-      //   "初音ミク",
-      //   // "VOCALOID",
-      //   // "オリキャラ",
-      //   // "らくがき",
-      //   "少女",
-      //   // "Fate/GrandOrder",
-      //   "おっぱい",
-      //   "FGO",
-      //   // "アイドルマスターシンデレラガールズ",
-      //   // "ケモノ",
-      //   "水着",
-      //   // "東方Project",
-      //   // "原创",
-      //   // "擬人化",
-      //   // "なにこれかわいい",
-      //   // "練習",
-      //   // "百合",
-      //   // "ラブライブ!",
-      //   "アイドルマスター",
-      //   "バーチャルYouTuber",
-      //   // "模写",
-      //   // "fanart",
-      //   // "魔法少女まどか☆マギカ",
-      //   "巨乳",
-      //   // "原創",
-      //   // "うごイラ",
-      //   // "girl",
-      //   // "風景",
-      //   // "ドット絵",
-      //   // "ファンタジー",
-      //   // "版権",
-      //   // "制服",
-      //   // "ハロウィン",
-      //   // "猫",
-      //   // "背景",
-      //   // "コピック",
-      //   // "original",
-      //   // "水彩",
-      //   "ロリ",
-      //   // "anime",
-      //   // "セーラー服",
-      //   // "illustration",
-      //   // "ホロライブ",
-      //   // "艦これかわいい",
-      //   // "けものフレンズ",
-      //   // "東方project",
-      //   // "うちの子",
-      //   // "VTuber",
-      //   // "色鉛筆",
-      //   "アズールレーン",
-      //   // "鏡音リン",
-      //   // "クリスマス",
-      //   // "デジタル",
-      //   "かわいい",
-      //   // "Original",
-      //   // "動物",
-      //   // "猫耳",
-      //   // "モノクロ",
-      //   // "チルノ",
-      //   // "女子高生",
-      //   // "アークナイツ",
-      //   // "描いてもいいのよ",
-      //   // "プリキュア",
-      //   // "3DCG",
-      //   // "art",
-      //   "原神",
-      //   // "ツインテール",
-      //   // "ラブライブ!サンシャイン!!",
-      //   // "明日方舟",
-      //   // "博麗霊夢",
-      //   // "ウマ娘プリティーダービー",
-      //   // "ガールズ&パンツァー",
-      //   // "霧雨魔理沙",
-      //   "ウマ娘",
-      //   // "少女前線",
-      //   "ふつくしい",
-      //   // "魅惑のふともも",
-      //   // "ドールズフロントライン",
-      //   "魅惑の谷間",
-      //   // "ガンダム",
-      //   // "ブラックマジシャンガール",
-      //   // "スク水",
-      //   // "フランドール・スカーレット",
-      //   // "グランブルーファンタジー",
-      //   "バニーガール",
-      //   "黒タイツ",
-      //   // "尻神様",
-      //   "極上の乳",
-      //   "おへそ",
-      //   // "プリンセスコネクト!Re:Dive",
-      //   // "仕事絵",
-      //   // "エロマンガ先生",
-      //   // "ゼノブレイド",
-      //   // "パンチラ",
-      //   "メイド",
-      //   "可愛い",
-      //   "ビキニ",
-      //   "ふともも",
-      //   "ぱんつ",
-      //   // "パンツ",
-      //   // "下着",
-      //   "尻",
-      // ];
-      // this.negativeTagArray = [
-      //   // "腐向け",
-      //   "腐",
-      //   // "ヘタリア",
-      //   // "ケモノ",
-      //   "刀剣乱舞",
-      //   // "おそ松さん",
-      //   // "女体化",
-      //   // "男の子",
-      //   // "ショタ",
-      //   // "鬼滅の刃",
-      //   // "僕のヒーローアカデミア",
-      //   // "黒子のバスケ",
-      //   // "銀魂",
-      //   // "進撃の巨人",
-      //   // "ハイキュー!!",
-      //   // "ワンピース",
-      //   // "獣人",
-      //   // "テニスの王子様",
-      //   // "戦国BASARA",
-      //   // "忍たま",
-      //   // "twst夢",
-      //   // "少年",
-      //   // "美男子",
-      //   // "イナズマイレブン",
-      //   // "デフォルメ",
-      //   // "にじさんじ",
-      //   // "描き方",
-      //   "講座",
-      //   // "作画資料",
-      //   // "創作",
-      //   // "素材",
-      //   "漫画",
-      //   // "SideM",
-      //   "メイキング",
-      //   "BL",
-      // ];
+  async function fetchImage(url) {
+    let res = await fetch(url);
+    return await res.blob();
+  }
+
+  let baseUrl = "https://www.pixiv.net";
+  let illustInfoUrl = "/ajax/illust/";
+  let searchUrl = "/ajax/search/illustrations/";
+
+  class SearchSource {
+    constructor() {
       this.positiveTagArray = [
         "7500users入り",
         "10000users入り",
@@ -331,7 +131,6 @@ chrome.runtime.onInstalled.addListener(() => {
       };
       this.totalPage = 300;
       this.itemsPerPage = 60;
-      this.searchUrl = "/ajax/search/illustrations/";
       this.illustInfoPages = {};
     }
 
@@ -371,184 +170,110 @@ chrome.runtime.onInstalled.addListener(() => {
 
     async searchIllustPage(p) {
       let paramUrl = this.generateSearchUrl(p);
-      let jsonResult = await this.fetchJson(this.baseUrl + this.searchUrl + paramUrl);
-      if (!jsonResult.body || !jsonResult.body.illust || !jsonResult.body.illust.data) {
-        throw new Error('empty search result');
-      }
+      let jsonResult = await fetchPixivJson(baseUrl + searchUrl + paramUrl);
       return jsonResult;
     }
 
     async getRandomIllust() {
       let randomPage = getRandomInt(0, Math.min(this.totalPage, 1000)) + 1;
       if (!this.illustInfoPages[randomPage]) {
-        try {
-          let pageObj = await this.searchIllustPage(randomPage);
-          let total = pageObj.body.illust.total;
-          let tp = Math.ceil(total / this.itemsPerPage);
-          if (tp > this.totalPage) {
-            this.totalPage = tp;
-          }
-          // filter sensitive images
-          // pageObj.body.illust.data = pageObj.body.illust.data.filter(
-          //   (el) => el.sl < 3
-          // );
-          this.illustInfoPages[randomPage] = pageObj.body.illust.data;
-        } catch (e) {
-          throw new Error(e);
+        let pageObj = await this.searchIllustPage(randomPage);
+        let total = pageObj.body.illust.total;
+        let tp = Math.ceil(total / this.itemsPerPage);
+        if (tp > this.totalPage) {
+          this.totalPage = tp;
         }
+        // filter sensitive images
+        // pageObj.body.illust.data = pageObj.body.illust.data.filter(
+        //   (el) => el.sl < 3
+        // );
+        this.illustInfoPages[randomPage] = pageObj.body.illust.data;
       }
       let illustArray = this.illustInfoPages[randomPage];
       let randomIndex = getRandomInt(0, illustArray.length);
-      if (!illustArray || !illustArray[randomIndex]) {
-        throw new Error('empty illustor info');
-      }
-      let res = {
-        illustId: illustArray[randomIndex].id,
-        profileImageUrl: illustArray[randomIndex].profileImageUrl
-      };
+      let res = {};
+      res.illustId = illustArray[randomIndex].id;
+      res.profileImageUrl = illustArray[randomIndex].profileImageUrl;
+      let illustInfo = await fetchPixivJson(baseUrl + illustInfoUrl + res.illustId);
+
+      res.userName = illustInfo.body.userName;
+      res.userId = illustInfo.body.userId;
+      res.illustId = illustInfo.body.illustId;
+      res.userIdUrl = baseUrl + "/users/" + illustInfo.body.userId;
+      res.illustIdUrl = baseUrl + "/artworks/" + illustInfo.body.illustId;
+      res.title = illustInfo.body.title;
+      res.imageObjectUrl = illustInfo.body.urls.regular;
+      await Promise.all([
+        fetchImage(res.imageObjectUrl)
+          .then((blob) => blobToDataUrl(blob))
+          .then((url) => { res.imageObjectUrl = url; }),
+        fetchImage(res.profileImageUrl)
+          .then((blob) => blobToDataUrl(blob))
+          .then((url) => { res.profileImageUrl = url; })
+      ]);
       return res;
     }
-
-    async getItem() {
-      let item = this.pop();
-      let tries = 0;
-      while (item === undefined) {
-        ++tries;
-        try {
-          await this.fill();
-        } catch (error) {
-          if (tries < this.maxRetries) {
-            await new Promise((resolve) => setTimeout(resolve, 500));
-          } else {
-            throw new Error(error);
-          }
-        } finally {
-          item = this.pop();
-        }
-      }
-      return item;
-    }
-
-    async fill() {
-      let tasks = [];
-      while (this.capacity() > this.size() + this.running) {
-        ++this.running;
-        tasks.push(new Promise((resolve, reject) => {
-          let res;
-          try {
-            res = this.getRandomIllust();
-            resolve(res);
-          } catch (e) {
-            reject(res);
-          }
-        }).then((res) =>
-          this.fetchJson(this.baseUrl + this.illustInfoUrl + res.illustId)
-            .then((illustInfo) => {
-              res.userName = illustInfo.body.userName;
-              res.userId = illustInfo.body.userId;
-              res.illustId = illustInfo.body.illustId;
-              res.userIdUrl = this.baseUrl + "/users/" + illustInfo.body.userId;
-              res.illustIdUrl = this.baseUrl + "/artworks/" + illustInfo.body.illustId;
-              res.title = illustInfo.body.title;
-              res.imageObjectUrl = illustInfo.body.urls.regular;
-              this.push(res);
-            })
-        ).catch((e) => console.log(e))
-          .finally(() => { --this.running; }));
-      }
-      return Promise.any(tasks);
-    }
-  }
-  class IllustFeeder extends illustSource {
-    constructor(maxsize = 2) {
-      super(maxsize);
-      this.sources = {
-        // discoverySource: new DiscoverySource(),
-        searchSource: new SearchSource()
-      }
-    }
-    async getItem() {
-      let item = this.pop();
-      let tries = 0;
-      while (item === undefined) {
-        ++tries;
-        try {
-          await this.fill();
-        } catch (error) {
-          if (tries < this.maxRetries) {
-            await new Promise((resolve) => setTimeout(resolve, 500));
-          } else {
-            throw new Error(error);
-          }
-        } finally {
-          item = this.pop();
-        }
-      }
-      return item;
-    }
-    async fill() {
-      let ss = Object.values(this.sources);
-      let tasks = [];
-      while (this.capacity() > this.size() + this.running) {
-        ++this.running;
-        tasks.push(new Promise((resolve) =>
-          resolve(ss[getRandomInt(0, ss.length)].getItem())
-        ).then((item) =>
-          Promise.all([
-            this.fetchImage(item.imageObjectUrl)
-              .then((blob) => {
-                return new Promise((resolve, reject) => {
-                  let reader = new FileReader();
-                  reader.onload = () => resolve(reader.result);
-                  reader.onerror = reject;
-                  reader.readAsDataURL(blob);
-                }).then((url) => {
-                  item.imageObjectUrl = url;
-                });
-              }),
-            this.fetchImage(item.profileImageUrl)
-              .then((blob) => {
-                return new Promise((resolve, reject) => {
-                  let reader = new FileReader();
-                  reader.onload = () => resolve(reader.result);
-                  reader.onerror = reject;
-                  reader.readAsDataURL(blob);
-                }).then((url) => {
-                  item.profileImageUrl = url;
-                });
-              })
-          ]).then(() => item)
-        ).then((item) => {
-          this.push(item);
-          chrome.storage.session.set({ illustFeederPool: this.array });
-        }).catch((e) => { console.log(e) })
-          .finally(() => { --this.running; }));
-      }
-      return Promise.any(tasks);
-    }
   }
 
-  const illustFeeder = new IllustFeeder();
-  cache = await chrome.storage.session.get("illustFeederPool");
-  illustFeeder.array = cache.illustFeederPool || [];
+  function blobToDataUrl(blob) {
+    return new Promise((resolve, reject) => {
+      let reader = new FileReader();
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = reject;
+      reader.readAsDataURL(blob);
+    });
+  }
+
+  let searchSource = new SearchSource();
+
+  queue_cache = await chrome.storage.session.get(["illustQueue"]);
+  let illust_queue;
+  if (Object.keys(queue_cache).length === 0) {
+    illust_queue = new Queue(3, []);
+  } else {
+    illust_queue = Object.setPrototypeOf(queue_cache.illustQueue, Queue.prototype)
+  }
+
+  let running = 0;
+  while (running < illust_queue.capacity() - illust_queue.size()) {
+    ++running;
+    setTimeout(async () => {
+      if (illust_queue.full()) { return; }
+      let res = await searchSource.getRandomIllust();
+      illust_queue.push(res);
+      chrome.storage.session.set({ illustQueue: illust_queue });
+      --running;
+    }, 0);
+  }
 
   chrome.runtime.onMessage.addListener(function (
     message,
     sender,
     sendResponse
   ) {
-    if (message.action === "fetchImage") {
-      illustFeeder.getItem()
-        .then((res) => {
+    (
+      async () => {
+        if (message.action === "fetchImage") {
+          let res = illust_queue.pop();
+          if (!res) {
+            res = await searchSource.getRandomIllust();
+          }
           sendResponse(res);
           console.log(res);
-        }).then(() => {
-          illustFeeder.fill();
-        }).catch((e) => {
-          sendResponse(null);
-          console.log(e);
-        });
-    }
+          while (running < illust_queue.capacity() - illust_queue.size()) {
+            ++running;
+            setTimeout(async () => {
+              if (illust_queue.full()) { return; }
+              let res = await searchSource.getRandomIllust();
+              illust_queue.push(res);
+              chrome.storage.session.set({ illustQueue: illust_queue });
+              --running;
+            }, 0);
+          }
+        }
+      }
+    )();
     return true;
   });
+  console.log("background script loaded");
 })();
